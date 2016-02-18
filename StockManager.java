2f0f0f9201a52f760d039ddb6c465fbb936f8514
francisco2016 <otrayama@gmail.com>
2016-02-18 20:09:53 +0100

    -0704 eliminada llamada a toString() en el mt findProduct()

--- StockManager.java
+++ StockManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Manage the stock in a business.
@@ -32,33 +33,58 @@ public class StockManager
     /**
      * Receive a delivery of a particular product.
      * Increase the quantity of the product by the given amount.
-     * @param id The ID of the product.
+     * @param id The ID of the product.------------------------------------------------------------------------------ 0064
      * @param amount The amount to increase the quantity by.
      */
     public void delivery(int id, int amount)
     {
+      Product producto = findProduct(id);
+      if(producto != null){
+         producto.increaseQuantity(amount);
+      }
+      else{
+        System.out.println("El id indicado no pertenece a ningun producto");
+      }
     }
+   
     
     /**
      * Try to find a product in the stock with the given id.
      * @return The identified product, or null if there is none
-     *         with a matching ID.
+     * return El producto identificado, o null si no hay ninguno
+     *         with a matching ID.------------------------------------------------------------------------------- 0064
      */
     public Product findProduct(int id)
     {
-        return null;
+      Product producto = null;
+      int index = 0;
+      boolean encontrado = false;
+      while(!encontrado && index < stock.size()){
+        if (stock.get(index).getID() == id)
+        {
+        	producto = stock.get(index);
+          encontrado = true;
+        }
+        index++;
+      }
+      return producto;
     }
     
     /**
      * Locate a product with the given ID, and return how
      * many of this item are in stock. If the ID does not
      * match any product, return zero.
-     * @param id The ID of the product.
+     * @param id The ID of the product.------------------------------------------------------------------------------ 0064
      * @return The quantity of the given product in stock.
      */
-    public int numberInStock(int id)
+     public int numberInStock(int id)
     {
-        return 0;
+			int cantidad = 0;
+      Product producto = findProduct(id); 
+      if (producto != null) {
+      	 cantidad = producto.getQuantity();
+      }
+      return cantidad;
     }
 
      /**
